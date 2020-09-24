04413c1346b26432c174e2f46d694d33d8d794a9
Fauzanadhim <fadzim018@gmail.com>
2020-09-25 05:55:38 +0700

    removing border

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -15,8 +15,8 @@ const checkLogin = (req,res,next)=>{
 // router.get("/", AdminController.getHomePage)
 // router.get("/delete/:productId", AdminController.getDeleteProcess) 
  
-router.get("/scanner", AdminController.getScanPage)  
-router.get("/scanner/pay?", AdminController.getScannedPage)  
+router.get("/scanner",checkLogin, AdminController.getScanPage)  
+router.get("/scanner/pay?",checkLogin, AdminController.getScannedPage)  
 router.get("/logout",checkLogin, AdminController.getLogout)
 router.get("/",checkLogin, AdminController.showAllProduct)
 router.get("/add",checkLogin, AdminController.addProductForm)
